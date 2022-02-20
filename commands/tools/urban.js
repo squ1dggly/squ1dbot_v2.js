a5c1d3c58eb7f4c45782fb4cfa3ff46bcb8ec9fe
squ1d <fluffyspiderstudios@gmail.com>
2022-02-20 12:22:39 -1000

    added slash command version for urban

--- commands/tools/urban.js
+++ commands/tools/urban.js
@@ -1,13 +1,15 @@
-// Asks the urban dictionary for the definition to a slang-like word.
+// Search the urban dictionary for a definition.
 
 const fetch = require('node-fetch');
-const { RandomChoice, RandomChance } = require('../../modules/jsTools');
+
 const { urban_CS } = require('../../embed_styles/commandStyles');
+const { RandomChoice, RandomChance } = require('../../modules/jsTools');
+const { urbanCommandTip, urbanEmptyResults } = require('../../configs/commandResponseList.json');
 
 const cmdName = "urban";
-const aliases = ["defineurban", "searchurban"];
+const aliases = [];
 
-const description = "Ask the urban dictionary for the definition to a slang-like word.";
+const description = "Search the urban dictionary for a definition.";
 
 module.exports = {
     name: cmdName,
@@ -15,27 +17,14 @@ module.exports = {
     description: description,
 
     execute: async (client, message, commandData) => {
-        // Randomized Responses
-        let commandTip_examples = [
-            `${commandData.guildData.guildPrefixes[0]}urban barack obama, 2`,
-            `${commandData.guildData.guildPrefixes[0]}urban pizza, 3`,
-            `${commandData.guildData.guildPrefixes[0]}urban 420 blaze it, 4`,
-            `${commandData.guildData.guildPrefixes[0]}urban rule34, 5`,
-            `${commandData.guildData.guildPrefixes[0]}urban the bite of 87, 6`,
-            `${commandData.guildData.guildPrefixes[0]}urban ratio, 7`,
-            `${commandData.guildData.guildPrefixes[0]}urban mald, 8`,
-            `${commandData.guildData.guildPrefixes[0]}urban cope, 9`,
-            `${commandData.guildData.guildPrefixes[0]}urban cheeseburger, 10`
-        ]
-        let commandTip = (RandomChance(3) ? "\n\n$TIP" : "")
-            .replace("$TIP", `Tip: You can view more than 1 definition by adding a comma (,) and then the number of definitions you want.\nExample: \`${RandomChoice(commandTip_examples)}\``);
-
-        let emptyResultsTaunt = [
-            "",
-            " Just like my parents did.",
-            " Just like my bank account. Smh",
-            " Just like my remaining brain cells after talking to you."
-        ]
+        let commandTip = (RandomChance(3) ? "\n\n$TIP" : "").replace(
+            "$TIP",
+            `Tip: You can view more than 1 definition by adding a comma (,) and then the number of definitions you want.
+                Example: \`${RandomChoice(urbanCommandTip
+                .replace("$PRFX", commandData.guildData.guildPrefixes[0])
+                .replace("$CMDNAME", cmdName))
+            }\``
+        );
 
         if (!commandData.args[0]) return await message.reply({
             content: `You imbecile. What am I even supposed to search?${commandTip}`
@@ -48,7 +37,7 @@ module.exports = {
         try {
             return await Search(term).then(async query => {
                 if (!query) return await message.reply({
-                    content: `Search results for \`${term}\` turned up empty.${RandomChoice(emptyResultsTaunt)}${commandTip}`
+                    content: `Search results for \`${term}\` turned up empty.${RandomChoice(urbanEmptyResults)}${commandTip}`
                 });
 
                 query = { searchedTerm: term, definitions: query };
---
+++ slash_commands/tools/urban_SLSH.js
@@ -0,0 +1,63 @@
+// Search the urban dictionary for a definition.
+
+const fetch = require('node-fetch');
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const { urban_CS } = require('../../embed_styles/commandStyles');
+const { RandomChoice, RandomChance } = require('../../modules/jsTools');
+const { urbanCommandTip, urbanEmptyResults } = require('../../configs/commandResponseList.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("urban")
+        .setDescription("Search the urban dictionary for a definition.")
+
+        .addStringOption(option => option.setName("keywords")
+            .setDescription("What you want to search.")
+            .setRequired(true))
+
+        .addNumberOption(option => option.setName("limit")
+            .setDescription("How many definitions would you like to see?"))
+
+        .addBooleanOption(option => option.setName("ephemeral")
+            .setDescription("If set to true only you will be able to see this message.")),
+
+    execute: async (client, interaction) => {
+        let term = interaction.options.getString("keywords");
+        let queryLimit = interaction.options.getNumber("keywords") | 1;
+        let ephemeral = interaction.options.getBoolean("ephemeral") || false;
+
+        if (!commandData.args[0]) return await interaction.reply({
+            content: "You imbecile. What am I even supposed to search?"
+        });
+
+        try {
+            return await Search(term).then(async query => {
+                if (!query) return await interaction.reply({
+                    content: `Search results for \`${term}\` turned up empty.${RandomChoice(urbanEmptyResults)}`
+                });
+
+                query = { searchedTerm: term, definitions: query };
+                let embed = urban_CS(query, queryLimit);
+
+                return await interaction.reply({ embeds: [embed] });
+            });
+        } catch (err) {
+            console.error(err);
+
+            return await interaction.reply({
+                content: "Failed to conduct search. Half our team didn't turn in today.",
+                ephemeral: ephemeral
+            });
+        }
+    }
+}
+
+// >> Custom Functions
+async function Search(term) {
+    let query = new URLSearchParams({ term });
+    let response = await fetch(`https://api.urbandictionary.com/v0/define?${query}`).then(r => r.json());
+
+    return (response.list && response.list.length > 0) ? response.list : null;
+}
